389e1a99d849483445f91b842b2ffe9a0ea739ce
BrenoNunes96 <brenocp3@live.com>
2026-04-16 12:02:25 -0300

    Service feita pages sobre e introdução(estrutura) e rotas parcial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Sobre from './pages/home/Sobre';
 
 function App() {
-
-
   return (
-    <>
-   
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
---
+++ src/pages/home/introducao_Projeto.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function introducao_Projeto() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
