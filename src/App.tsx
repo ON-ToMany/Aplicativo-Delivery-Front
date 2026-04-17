08c059764ca1f005c7114d7c9191d1959b9f80a7
mayaramonteiro <imayagmb@gmail.com>
2026-04-17 03:37:38 -0300

    feat: cria página Sobre Nós com seção OneToMany e Equipe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from "./components/footer/Footer";
+import Equipe from './pages/sobre/equipe/Equipe';
+import SobreOneToMany from './pages/sobre/sobre-one-to-many/SobreOneToMany';
+import Sobre from './pages/sobre/Sobre';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/inicio" element={<Home />} />
+        <Route path="/sobre" element={<Sobre/>} />
+        <Route path="/sobre" element={<Equipe/>} />
+        <Route path="/sobre" element={<SobreOneToMany/>} />
       </Routes>
 
       <Footer />
--- src/pages/sobre/sobre-one-to-many/SobreOneToMany.tsx
+++ src/pages/sobre/sobre-one-to-many/SobreOneToMany.tsx
@@ -1,7 +1,38 @@
 function SobreOneToMany() {
   return (
-    <div>SobreOneToMany</div>
-  )
+    <section
+      id="sobre"
+      className="pt-32 pb-20 px-6 bg-[#E2BE94]"
+    >
+      <div className="max-w-7xl mx-auto">
+
+        <div className="flex flex-col md:flex-row items-center gap-12">
+
+          {/* IMAGEM */}
+          <div className="flex justify-center shrink">
+            <img
+              src="https://ik.imagekit.io/9yqf3fqpw/OneToMany..png"
+              alt="OneToMany"
+              className="w-72 object-contain"
+            />
+          </div>
+
+          {/* TEXTO */}
+          <div className="max-w-md ml-50">
+            <h2 className="text-4xl font-bold text-red-950 mb-6">
+              Sobre Nós
+            </h2>
+
+            <p className="text-red-950 text-lg leading-relaxed text-justify">
+              Uma empresa focada no desenvolvimento de soluções tecnológicas
+              com propósito.
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default SobreOneToMany
+export default SobreOneToMany;
