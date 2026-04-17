d438ea3bc4916c291de3558debd3852c87a39eb4
Maria Eduarda <mariaeduardao.gms@gmail.com>
2026-04-16 22:08:13 -0300

    feat: implementa navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,32 @@
+import { RiUserFill } from "react-icons/ri";
+import Logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <div className="sticky flex flex-row justify-between items-center p-4 bg-[#E2BE94] border-l border-r border-b rounded-b-3xl border-red-900 text-red-900 font-semibold shadow-sm">
+        <Link to="/">
+          <div className="flex flex-row justify-center items-center gap-2">
+            <img src={Logo} alt="Logo" className="h-12" />
+            <span className="text-2xl text-red-800 font-extrabold italic">FRIENDLY FOOD</span>
+          </div>
+        </Link>
+        <nav>
+          <ul className="flex flex-row gap-6">
+            <li className="hover:text-red-950 transition-colors duration-200"><a href="/">Início</a></li>
+            <li className="hover:text-red-950 transition-colors duration-200"><a href="/cardapio">Cardápio</a></li>
+            <li className="hover:text-red-950 transition-colors duration-200"><a href="/onde-estamos">Onde Estamos</a></li>
+            <li className="hover:text-red-950 transition-colors duration-200"><a href="/sobre">Sobre</a></li>
+          </ul>
+        </nav>
+
+        <div className="w-fit h-fit flex felx-row gap-4 items-center">
+          <button className="w-fit p-2 border border-red-900 bg-yellow-50 rounded-full shadow-sm font-medium cursor-pointer text-nowrap hover:border-red-950 hover:text-red-950 hover:scale-103 transition-all duration-200">Faça seu pedido</button>
+          <button className="w-full h-full rounded-full bg-red-900 text-yellow-50 text-2xl p-2 cursor-pointer hover:bg-red-950 hover:scale-103 transition-all  duration-200 "><RiUserFill /></button>
+        </div>
+      </div>
+    </>
   );
 }
 
